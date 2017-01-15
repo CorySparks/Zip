f7691d105c7acc92e8e8a85170c1749ce5059e29
Cory Sparks <sparks-40067@cotc.edu>
2017-01-14 23:38:33 -0500

    Node.js Express

---
+++ Zip-API/app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var monk = require('monk');
+var session = require('client-sessions');
+var db = monk('localhost:27017/ZipDB');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  req.db = db;
+
+  next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+    console.log(err);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.get('/loginUser', function(req, res){
+  if(req.session && req.session.user){
+    User.findOne(({'username.search': username.toLowerCase()}), function (err, user){
+      if(!user){
+        req.session.reset();
+        res.send('0');
+      }else{
+        res.send('1');
+      }
+    });
+  }else{
+    res.send('0');
+  }
+});
+
+
+module.exports = app;
